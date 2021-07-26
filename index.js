a15ef70479338359fc6c519006745008f2388b32
Ayoub Mechouat <ayoub.mechouat@gmail.com>
2021-07-26 14:44:09 +0200

    backend ready

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const formidable = require("express-formidable");
+const data = require("./data");
+
+const app = express();
+app.use(cors());
+app.use(formidable());
+
+app.get("/", (req, res) => {
+  res.status(200).json(data);
+});
+
+app.all("*", (req, res) => {
+  res.status(400).json({ message: "Page not found" });
+});
+
+app.listen(3000, () => {
+  console.log("Server started");
+});
